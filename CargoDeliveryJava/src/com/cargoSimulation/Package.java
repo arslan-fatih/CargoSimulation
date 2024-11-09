36d707f5f40cf043d12a67f2c445f27fad24dcf1
Fatih Arslan <arslanfatih929@gmail.com>
2024-11-10 01:42:42 +0300

    package class updated

--- CargoDeliveryJava/src/com/cargoSimulation/Package.java
+++ CargoDeliveryJava/src/com/cargoSimulation/Package.java
@@ -1,5 +1,27 @@
 package com.cargoSimulation;
 
 public class Package {
+	private String id;
+	private String city;
+	public Package(String id, String city) {
+		super();
+		this.id = id;
+		this.city = city;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+	
 
 }
